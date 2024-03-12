6e4d54a931eeff3696b4f2d623f9c1e0895a4313
Pisarski Roman <133641494+Romanooo84@users.noreply.github.com>
2024-03-12 21:36:32 +0100

    start

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-import './js/fetchMovies.js';
-import './js/moviesContainer.js';
-import './js/footerStudents.js';
-import './js/add-to-queue.js'; // ymczasowy błąd 404
-import './js/watched.js';
-import './js/fetchKeyWords';
 import './sass/main.scss';
