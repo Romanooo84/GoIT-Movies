78dcce2d68960fa53d59aca5b9971c59572ac00b
Weronika Niemczyk <112780270+WeronikaNiemczyk@users.noreply.github.com>
2024-03-13 17:30:44 +0100

    fetchKeyWords - Updated
    Dopracowany plik z wyszukiwaniem po słowie kluczowym. Należy dodać do niego tylko paginację.
    Dodatkowo dodany genres - on jest mi potrzebny po to aby zaczytywało gatunek filmu (tip od Ivana) oraz plik queueWatchedLibrary - narazie całkiem roboczo, czekam na Sebastiana.

--- src/js/fetchKeyWords.js
+++ src/js/fetchKeyWords.js
@@ -1,3 +1,4 @@
+import { genresId } from './genres';
 import Notiflix from 'notiflix';
 const searchForm = document.querySelector('.header-home-form');
 const moviesContainer = document.querySelector('.movie__list');
@@ -18,46 +19,42 @@ const fetchKeyMovies = async (querySearch, page) => {
   return responseKeyMovies.results;
 };
 
-// const fetchGenres = async () => {
-//   const API_KEY = 'e7c930d9ee21da94f8fc3257d387eced';
-//   const searchParamsGenres = new URLSearchParams({
-//     api_key: API_KEY,
-//   });
-
-//   const genres = await fetch(`https://api.themoviedb.org/3/genre/movie/list?${searchParamsGenres}`);
-//   const genresNames = await genres.json();
-//   console.log('genresnames', genresNames);
-//   return genresNames;
-// };
-// console.log(fetchGenres());
-
 const renderKeyMovies = movies => {
   console.log('Movies', movies);
 
   return movies
     .map(({ id, poster_path, original_title, genre_ids, release_date }) => {
+      let filmGenreId = '';
+      if (genre_ids && genre_ids.length > 0) {
+        filmGenreId = genresId
+          .filter(({ id }) => genre_ids.includes(id))
+          .map(({ name }) => name)
+          .join(', ');
+      } else {
+        filmGenreId = 'Genre is not available';
+      }
       return `<li class="movie-card">
-      <a href="${poster_path}" data-movie-id="${id}">
-        <img src="${
-          poster_path
-            ? `https://image.tmdb.org/t/p/w500${poster_path}`
-            : 'https://cdn.vectorstock.com/i/1000x1000/82/99/no-image-available-like-missing-picture-vector-43938299.webp'
-        }" alt="${original_title}"/>
-      </a>
-      <div class="info">
-        <p class="info-item">
-          <b> ${original_title}</b>
-        </p>
-        <div class="details">
-        <p class="info-item">
-        <b>${genre_ids.slice(0, 2)}</b>
-      </p>
-        <p class="info-item">
-          <b>| ${release_date.slice(0, 4)}</b>
-        </p>
-        </div>
-      </div>
-    </li>`;
+            <a href="${poster_path}" data-movie-id="${id}">
+              <img src="${
+                poster_path
+                  ? `https://image.tmdb.org/t/p/w500${poster_path}`
+                  : 'https://icrier.org/wp-content/uploads/2022/09/Event-Image-Not-Found.jpg'
+              }" alt="${original_title}"/>
+            </a>
+            <div class="info">
+              <p class="info-item">
+                <b> ${original_title}</b>
+              </p>
+              <div class="details">
+              <p class="info-item">
+              <b>${filmGenreId}</b>
+            </p>
+              <p class="info-item">
+                <b>| ${release_date.slice(0, 4)}</b>
+              </p>
+              </div>
+            </div>
+          </li>`;
     })
     .join('');
 };
@@ -84,3 +81,4 @@ const searchingInput = async event => {
   searchForm.reset();
 };
 searchForm.addEventListener('submit', searchingInput);
+
--- src/js/queueWatchedLibrary.js
+++ src/js/queueWatchedLibrary.js
@@ -1,6 +1,73 @@
 // ** na postawie pull requesta Sebastiana, który dodaje filmy do kolejki i obejrzane w local storage ** //
 
-const libraryBox = document.querySelector('.main-library-home');
+const libraryBoxWatched = document.querySelector('.library-watched');
+const libraryBoxQueued = document.querySelector('.library-queue');
+const watchedButton = document.querySelector('.header-library-button-watched');
+const queueButton = document.querySelector('.header-library-button-queue');
+
+const showWatched = () => {};
+const showQueued = () => {};
+
+watchedButton.addEventListener('click', showWatched);
+queueButton.addEventListener('click', showQueued);
+
+// *** WERSJA watched z przyciskiem*** //
+document.addEventListener('DOMContentLoaded', () => {
+  const watchedMovies = JSON.parse(localStorage.getItem('watchedMovies')) || [];
+  const libraryBoxWatched = document.querySelector('.library-watched');
+
+  // Funkcja tworząca HTML dla filmów
+  const createMovieElements = () => {
+    return watchedMovies
+      .map(({ id, poster_path, original_title, genre_ids, release_date }) => {
+        return `<li class="movie-card">
+          <a href="${poster_path}" data-movie-id="${id}">
+            <img src="${
+              poster_path
+                ? `https://image.tmdb.org/t/p/w500${poster_path}`
+                : 'https://cdn.vectorstock.com/i/1000x1000/82/99/no-image-available-like-missing-picture-vector-43938299.webp'
+            }" alt="${original_title}"/>
+          </a>
+          <div class="info">
+            <p class="info-item">
+              <b>${original_title}</b>
+            </p>
+            <div class="details">
+              <p class="info-item">
+                <b>${genre_ids.slice(0, 2)}</b>
+              </p>
+              <p class="info-item">
+                <b>| ${release_date.slice(0, 4)}</b>
+              </p>
+            </div>
+          </div>
+        </li>`;
+      })
+      .join('');
+  };
+
+  // Funkcja wyświetlająca filmy w galerii
+  const displayWatchedMovies = () => {
+    libraryBoxWatched.innerHTML = createMovieElements();
+  };
+
+  // Wywołaj funkcję do wyświetlania filmów, gdy strona się załaduje
+  displayWatchedMovies();
+
+  const watchedButton = document.querySelector('.header-library-button-watched');
+  if (watchedButton) {
+    watchedButton.addEventListener('click', () => {
+      // const movieId = watchedButton.getAttribute('data-movie-id');
+      // addToWatched(movieId); // Dodaj film do obejrzanych
+      // watchedMovies.push(movieId); // Dodaj film do listy obejrzanych filmów
+      // localStorage.setItem('watchedMovies', JSON.stringify(watchedMovies)); // Zapisz zmienioną listę w local storage
+      // Ponownie wyświetl filmy w galerii po dodaniu nowego filmu
+      displayWatchedMovies();
+    });
+  }
+});
+
+// *** *** //
 
 libraryBox.addEventListener('DOMContentLoaded', () => {
   const queueMovies = JSON.parse(localStorage.getItem('queueMovies')) || [];
@@ -39,3 +106,4 @@ libraryBox.addEventListener('DOMContentLoaded', () => {
   });
   moviesList.appendChild(movieShow);
 });
+
