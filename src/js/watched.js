9fcc5071bc18ce3d5d686aee458a7e1f37c6684e
Weronika Niemczyk <112780270+WeronikaNiemczyk@users.noreply.github.com>
2024-03-14 19:33:52 +0100

    add to watched / queue

--- src/js/watched.js
+++ src/js/watched.js
@@ -1,30 +1,17 @@
-<<<<<<< Updated upstream
-document.addEventListener('DOMContentLoaded', () => {
-  const watchedBtn = document.getElementById('watched-btn');
-  if (watchedBtn) {
-    watchedBtn.addEventListener('click', () => {
-      const movieId = watchedBtn.getAttribute('data-movie-id');
-      addToWatched(movieId);
-    });
-  }
-});
-
-function addToWatched(movieId) {
-=======
 const addToWatched = event => {
   const movieId = event.target.getAttribute('data-movie-id');
 
   if (typeof Storage !== 'undefined') {
-    const moviesInStorage = localStorage.getItem('movies');
-    let movies = [];
+    const moviesInStorage = localStorage.getItem('watched');
+    let watched = [];
 
     if (moviesInStorage) {
-      movies = JSON.parse(moviesInStorage);
+      watched = JSON.parse(moviesInStorage);
     }
 
-    movies.push({ id: movieId });
+    watched.push({ id: movieId });
 
-    localStorage.setItem('movies', JSON.stringify(movies));
+    localStorage.setItem('watched', JSON.stringify(watched));
 
     alert('Film został dodany do listy oglądanych!');
   } else {
@@ -37,29 +24,3 @@ document.addEventListener('click', () => {
   watchedButton.addEventListener('click', addToWatched);
 });
 
-/*const addToWatched = movieId => {
->>>>>>> Stashed changes
-  const watched = JSON.parse(localStorage.getItem('watchedMovies')) || [];
-  if (!watched.includes(movieId)) {
-    watched.push(movieId);
-    localStorage.setItem('watchedMovies', JSON.stringify(watched));
-  }
-}
-
-/*const addToWatched = movieId => {
-  const watched = JSON.parse(localStorage.getItem('watchedMovies')) || [];
-  if (!watched.includes(movieId)) {
-    watched.push(movieId);
-    localStorage.setItem('watchedMovies', JSON.stringify(watched));
-  }
-};*/
-
-/*document.addEventListener('DOMContentLoaded', () => {
-  const addToWatchedBtn = document.getElementById('add-to-watched');
-  if (addToWatchedBtn) {
-    addToWatchedBtn.addEventListener('click', () => {
-      const movieId = addToWatchedBtn.getAttribute('data-movie-id');
-      addToWatched(movieId);
-    });
-  }
-}); */
