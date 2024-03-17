262715e6007c1d2655361d75662dd441115ddc05
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2024-03-17 10:54:46 +0100

    Update moviesContainer.js
    fixed

--- src/js/moviesContainer.js
+++ src/js/moviesContainer.js
@@ -105,7 +105,7 @@ videoSection.addEventListener('click', async e => {
       <dd>${movieDetails.overview}</dd>
       </div>
       <div class="modal-button-window">
-      <button class="button-standard modal-button" data-movie-id="${movieId}" type="button" class="add-to-watched" id="watched-btn">ADD TO WATCHED</button>
+      <button class="button-standard modal-button modal-button__queue" data-movie-id="${movieId}" type="button" class="add-to-watched" id="watched-btn">ADD TO WATCHED</button>
       <button class="button-standard modal-button modal-button__queue" data-movie-id="${movieId}" type="button" class="add-to-queue" id="queue-button">ADD TO QUEUE</button>
       </div>
     </div>
